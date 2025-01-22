c4b8b3374ce2781e60cd4aa065983b64f33cad39
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-01-22 16:18:06 +0530

    working on knight movement logic

---
+++ knightTravails.js
@@ -0,0 +1,7 @@
+//  you can either + or - the points
+//  + leads to right for column 
+//  - leads to left for column
+//  can only perform one at a time
+//  + leads to down for row
+//  - leads to up for row
+//  the sequence of these moves also matters
